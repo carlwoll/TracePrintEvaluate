d2c8f578b30a8e521b4a00a11eb2c4101acf5806
Carl Woll <carlw@wolfram.com>
2018-07-01 09:28:01 -0400

    Actually commit TPE changes

--- TracePrintEvaluate/TracePrintEvaluate.m
+++ TracePrintEvaluate/TracePrintEvaluate.m
@@ -87,7 +87,7 @@ TracePrintEvaluate[expr_, form_, opts:OptionsPattern[]] := Module[
 					TraceInternal->OptionValue[TraceInternal]
 				];
 				
-				"\[Bullet]",
+				RawBoxes @ "\[Bullet]",
 				Initialization :> (values = vv; timing = tt; outAction = oo)
 			];
 
@@ -95,6 +95,7 @@ TracePrintEvaluate[expr_, form_, opts:OptionsPattern[]] := Module[
 				CellPrint @ Cell[
 					BoxData @ ToBoxes @ res,
 					"PersistentOutput",
+					CellMargins -> CurrentValue[{StyleDefinitions, "Output", CellMargins}],
 					CellLabel -> "Out["<>ToString[$Line]<>"]=",
 					CellDingbat -> Cell @ BoxData @ ToBoxes @ r
 				];
@@ -112,7 +113,10 @@ ToggleSelector /: MakeBoxes[ToggleSelector[{in_, out__}, init_Integer:1, Options
 	name = "TPE" <> ToString@Length[Hold[in, out]],
 	inboxes = MakeBoxes[in, fmt],
 	outboxes = ToBoxes /@ Hold[out],
-	df = If[TrueQ @ OptionValue[ToggleSelector, "Stylesheet"], Sequence@@{}, DisplayFunction -> TPEDisplayFunction[Length[{in, out}]]]
+	df = If[TrueQ @ OptionValue[ToggleSelector, "Stylesheet"] && Length@Hold[in, out]<=5,
+		Sequence@@{},
+		DisplayFunction -> TPEDisplayFunction[Length[{in, out}]]
+	]
 	},
 
 	Replace[
@@ -163,7 +167,7 @@ styles = Append[
 			CellContext->Cell,
 			TemplateBoxOptions->{DisplayFunction->TPEDisplayFunction[n]}
 		],
-		{n, 7}
+		{n, 5}
 	],
 	Cell[StyleData["PersistentOutput", StyleDefinitions->StyleData["Output"]]]
 ]
