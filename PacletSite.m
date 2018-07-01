f93a05979b43080cae8fa5d10fec542978f38180
Carl Woll <carlw@wolfram.com>
2018-06-30 20:04:25 -0400

    Restore PacletSite.m

--- PacletSite.m
+++ PacletSite.m
@@ -1,1 +1,1 @@
-PacletSite[Paclet[ Name -> "TracePrintEvaluate", Version -> "0.0.1", MathematicaVersion -> "6+", Extensions -> { {"Documentation", Language -> "English"} }]]
+PacletSite[Paclet[ Name -> "TracePrintEvaluate", Version -> "0.0.1", MathematicaVersion -> "11+", Description -> "TracePrintEvaluate is like TracePrint, but it also embeds the function output (and possibly other data) into the trace prints.", Creator -> "Carl Woll", Extensions -> { {"Documentation", Language -> "English"}, {"Kernel", Root -> ".", Context -> "TracePrintEvaluate`"} } ] ]
